ba0d63a474de4528c001109a6e2bad56ed765fac
awalone <whoamikimimaki@gmail.com>
2015-08-18 15:04:44 -0700

    tes commit

--- application/views/izin_iup/cari_bidang.php
+++
@@ -1,9 +0,0 @@
-<table class="tabelisi tabelisisimbad"  style="font:bold">
-<tr><td>Cari:
-<input type="text" name="cari" id="carilangsung" onkeypress="if(event.keyCode == 13) {document.getElementById('buttonCari').click(); return false;}">
-<input type="submit" id="buttonCari" value="Ok" onClick="fcr = document.getElementById('carilangsung'); lempar= document.getElementById('carilangsung'); 
-send='lempar='+lempar.value; ubahisi('isifilter','<?php echo base_url();?>index.php/izin_iup/groupBidang',send);">
-</td></tr>
-</table>
-<div id="isifilter" style="max-height:200px; overflow-y:scroll; position:relative">
-</div>
--- application/views/pegawai/assets_view.php
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery.dataTables.bootstrap.js"></script>
-
-<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery.dataTables.min.js"></script>
